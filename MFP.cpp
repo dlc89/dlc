a36025f22c51ef5d37fbf280aeb90c6a3ece6500
dlc89 <ldeec57@hotmail.com>
2017-08-16 02:02:22 -0700

    Update MFP.cpp

--- MFP.cpp
+++ MFP.cpp
@@ -1,10 +1,10 @@
-#include "MyFirstPlugin.h"
+#include "MYF.h"
 #include "IPlug_include_in_plug_src.h"
 #include "IControl.h"
 #include "resource.h"
-#include <math.h>
+#include <cmath>
 
-#include <algorithm>
+
 const int kNumPrograms = 1;
 
 enum EParams
@@ -18,45 +18,36 @@ enum ELayout
   kWidth = GUI_WIDTH,
   kHeight = GUI_HEIGHT,
 
-  kGainX = 27,
-  kGainY = 62,
-  kKnobFrames = 128
+  kGainX = 100,
+  kGainY = 100,
+  kKnobFrames = 60
 };
 
-MyFirstPlugin::MyFirstPlugin(IPlugInstanceInfo instanceInfo)
+MYF::MYF(IPlugInstanceInfo instanceInfo)
   :	IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo), mGain(1.)
 {
   TRACE;
 
   //arguments are: name, defaultVal, minVal, maxVal, step, label
-  GetParam(kGain)->InitDouble("Gain", 0.0, 0., 100.0, 0.01, "%","Group");//Iparam class starts the chain 
-  //InitDouble is an Iparam class method and GetParam is from Iplugbase.h with an Iparam class that has a Iplugbase::getparampointer 
-  GetParam(kGain)->SetShape(1.0); 
+  GetParam(kGain)->InitDouble("Gain", 50., 0., 100.0, 0.01, "%");
+  GetParam(kGain)->SetShape(2.);
 
-
-  IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight); // setting pointer for later AttachGraphics(pGraphics);
-  pGraphics->AttachBackground(BLACKJA_ID, BLACKJA_FN);
+  IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight);
+  pGraphics->AttachPanelBackground(&COLOR_RED);
 
   IBitmap knob = pGraphics->LoadIBitmap(KNOB_ID, KNOB_FN, kKnobFrames);
 
   pGraphics->AttachControl(new IKnobMultiControl(this, kGainX, kGainY, kGain, &knob));
-  /*pGraphics->AttachControl(new IKnobMultiControl(this, 300, 90, kGain, &knob));
-  pGraphics->AttachControl(new IKnobMultiControl(this, 600, 80, kGain, &knob));
-  pGraphics->AttachControl(new IKnobMultiControl(this, kGainX, 430, kGain, &knob));
-  pGraphics->AttachControl(new IKnobMultiControl(this, 300, 430, kGain, &knob));
-  pGraphics->AttachControl(new IKnobMultiControl(this, 600, 430, kGain, &knob));
-*/
 
   AttachGraphics(pGraphics);
 
   //MakePreset("preset 1", ... );
-  MakeDefaultPreset((char *) "-", kNumPrograms); 
+  MakeDefaultPreset((char *) "-", kNumPrograms);
 }
 
-MyFirstPlugin::~MyFirstPlugin() {}
+MYF::~MYF() {}
 
-void MyFirstPlugin::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames)  
-
+void MYF::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames)
 {
   // Mutex is already locked for us.
 
@@ -64,35 +55,31 @@ void MyFirstPlugin::ProcessDoubleReplacing(double** inputs, double** outputs, in
   double* in2 = inputs[1];
   double* out1 = outputs[0];
   double* out2 = outputs[1];
-  //double mPhase =0 ; double mPhaseIncrement = 0 ;
-  //const double mPI= 0;
-  //const double twoPI = 2 * mPI;
 
-
-  for (int s = 0; s < nFrames;s++,out1++)
+  for (int s = 0; s < nFrames; ++s, in1++, in2++, ++out1, ++out2)
   {
+	  //out1[s]= sin(0.5);
 
+	  while (*in1 > *out1) {
+		  
+		  double *ipp = out1; 
+		  *ipp = s *  *in1;
 
-  out1[s]= sin(.66) * mGain ;
-  // *out1 and out1[s]
+		  *out1 = *in1  * mGain;
+		  *out2 = *in2 * *ipp * mGain;
+		  *out1 += *in1;
 
-
-   
+	  }
+  }
 }
-  
-}
 
-
-
-
-
-void MyFirstPlugin::Reset()
+void MYF::Reset()
 {
   TRACE;
   IMutexLock lock(this);
 }
 
-void MyFirstPlugin::OnParamChange(int paramIdx)
+void MYF::OnParamChange(int paramIdx)
 {
   IMutexLock lock(this);
 
